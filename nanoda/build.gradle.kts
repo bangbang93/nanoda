497b7678d3b868daebc9bd7b36f47e00d38f0ae0
bangbang93 <bangbang93@163.com>
2026-02-14 08:58:30 +0800

    chore: setup jreleaser

--- nanoda/build.gradle.kts
+++ nanoda/build.gradle.kts
@@ -1,4 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jreleaser.gradle.plugin.tasks.AbstractJReleaserTask
+import org.jreleaser.model.Active
+import org.jreleaser.model.Signing
 
 plugins {
   kotlin("jvm") version libs.versions.kotlin.get()
@@ -8,6 +11,8 @@ plugins {
   alias(libs.plugins.kotest)
   alias(libs.plugins.ktfmt)
   alias(libs.plugins.detekt)
+  alias(libs.plugins.jreleaser)
+  `maven-publish`
 }
 
 dependencies {
@@ -52,4 +57,80 @@ java {
 kotlin {
   jvmToolchain(21)
   compilerOptions { jvmTarget = JvmTarget.JVM_17 }
+}
+
+jreleaser {
+  gitRootSearch = true
+  release {
+    github {
+      overwrite = true
+      uploadAssets = Active.ALWAYS
+    }
+  }
+  signing {
+    active = Active.ALWAYS
+    pgp { mode = Signing.Mode.COMMAND }
+  }
+  deploy {
+    maven {
+      mavenCentral {
+        create("sonatype") {
+          active = Active.ALWAYS
+          url = "https://central.sonatype.com/api/v1/publisher"
+          stagingRepositories = listOf("build/staging-deploy")
+        }
+      }
+    }
+  }
+}
+
+tasks.withType<AbstractJReleaserTask>().configureEach {
+  notCompatibleWithConfigurationCache("https://github.com/jreleaser/jreleaser/issues/1992")
+}
+
+java {
+  withSourcesJar()
+  withJavadocJar()
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+
+      pom {
+        name = "nanoda"
+        description = "nanoda is a Kotlin utility library"
+        url = "https://github.com/bangbang93/nanoda"
+        inceptionYear = "2026"
+
+        licenses {
+          license {
+            name.set("MIT License")
+            url.set("https://opensource.org/licenses/MIT")
+          }
+        }
+
+        developers {
+          developer {
+            id.set("bangbang93")
+            name.set("bangbang93")
+          }
+        }
+
+        scm {
+          connection.set("scm:git:git://github.com/bangbang93/nanoda.git")
+          developerConnection.set("scm:git:ssh://git@github.com:bangbang93/nanoda.git")
+          url.set("https://github.com/bangbang93/nanoda")
+        }
+      }
+    }
+  }
+
+  repositories {
+    maven {
+      name = "localStaging"
+      url = uri(layout.buildDirectory.dir("staging-deploy"))
+    }
+  }
 }

fbdc5def18be51227acd03d655b7829bf4e0e941
bangbang93 <bangbang93@163.com>
2026-02-14 14:35:26 +0800

    fix: 修正异常定义

--- nanoda/src/main/kotlin/com/bangbang93/nanoda/exception/ServiceException.kt
+++ nanoda/src/main/kotlin/com/bangbang93/nanoda/exception/ServiceException.kt
@@ -12,11 +12,12 @@ open class ServiceException(
     @JvmOverloads
     fun from(
         definition: ExceptionDefinition,
+        message: String? = null,
         data: Map<Any, Any>? = null,
         cause: Throwable? = null,
     ): ServiceException =
         ServiceException(
-            message = definition.message,
+            message = message ?: definition.message,
             code = definition.code,
             httpCode = definition.httpCode,
             data = data,
@@ -31,47 +32,40 @@ open class ServiceException(
       message: String,
       cause: Throwable? = null,
       data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_UNKNOWN", 500, data)
+  ) : ServiceException(message, "COMMON_UNKNOWN", 500, data, cause)
 
   /** 需要登录 */
   class NeedLoginServiceException(
       message: String,
       cause: Throwable? = null,
       data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_NEED_LOGIN", 401, data)
-
-  /** 组织错误 */
-  class WrongOrganizationServiceException(
-      message: String,
-      cause: Throwable? = null,
-      data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_WRONG_ORGANIZATION", 400, data)
+  ) : ServiceException(message, "COMMON_NEED_LOGIN", 401, data, cause)
 
   /** 找不到对象 */
   class NotFoundServiceException(
       message: String,
       cause: Throwable? = null,
       data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_NOT_FOUND", 404, data)
+  ) : ServiceException(message, "COMMON_NOT_FOUND", 404, data, cause)
 
   /** 权限不足 */
   class PermissionDeniedServiceException(
       message: String,
       cause: Throwable? = null,
       data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_PERMISSION_DENIED", 403, data)
+  ) : ServiceException(message, "COMMON_PERMISSION_DENIED", 403, data, cause)
 
   /** 参数缺失 */
   class MissingArgumentServiceException(
       message: String,
       cause: Throwable? = null,
       data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_MISSING_ARGUMENT", 400, data)
+  ) : ServiceException(message, "COMMON_MISSING_ARGUMENT", 400, data, cause)
 
   /** 参数无效 */
   class InvalidArgumentServiceException(
       message: String,
       cause: Throwable? = null,
       data: Map<Any, Any>? = null,
-  ) : ServiceException(message, "COMMON_INVALID_ARGUMENT", 400, data)
+  ) : ServiceException(message, "COMMON_INVALID_ARGUMENT", 400, data, cause)
 }
